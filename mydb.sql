2a5a97a8aee184e51d3374edc8ea21b1ebaea29a
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-06-29 01:44:51 -0700

    update

--- mydb.sql
+++ mydb.sql
@@ -1,4 +1,3 @@
-
 /** Grocery list:
 Bananas (4)
 Peanut Butter (1)
@@ -8,3 +7,16 @@ Dark Chocolate Bars (2)
 CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
 
 INSERT INTO groceries VALUES (1, "Bananas", 4);
+INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
+INSERT INTO groceries VALUES (3, "Dark chocolate bars", 2);
+
+
+-- sql queries use select.
+SELECT name FROM groceries;
+
+SELECT * FROM groceries WHERE aisle > 5 ORDER BY aisle;
+
+-- SQL engine first does grouping of the rows based on aisle. So first group by, THEN sums up the quantity in each of those groups, then finally it selects teh first aisle value that it saw in each group. 
+SELECT aisle, SUM(quantity) FROM groceries GROUP BY aisle;
+
+-- Aggregate function-> useful for things like getting the max/min, sum, average of values in our database
