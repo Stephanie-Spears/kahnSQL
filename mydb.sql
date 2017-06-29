99d6573abee3d99f709a9f21b67132050c16bbf7
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-06-29 00:29:34 -0700

    kahn academy first list

---
+++ mydb.sql
@@ -0,0 +1,10 @@
+
+/** Grocery list:
+Bananas (4)
+Peanut Butter (1)
+Dark Chocolate Bars (2)
+**/
+
+CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
+
+INSERT INTO groceries VALUES (1, "Bananas", 4);
